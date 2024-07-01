ea3dd759bd145b876074f165ffaa2f71f7d1da03
ZHH <zhang2ha@qq.com>
2024-07-01 10:42:09 +0800

    feat(docs): add initial stories and hooks documentation
    - Create Icon stories with different variations like Component, Style, and Spin.
    - Add test, test2, test3 stories for testing purposes.
    - Document useCookie, useCountDown, useHover, and useScrolling hooks.

---
+++ docs/src/stories/useHover.stories.tsx
@@ -0,0 +1,31 @@
+import type { StoryObj } from "@storybook/react";
+import { useHover } from "zhh-hooks";
+
+const meta = {
+  title: "Hooks/useHover",
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Example: Story = {
+  args: {},
+  render: () => {
+    const element = (hovered: boolean) => (
+      <span>Hover me! {hovered && "Thanks! "}</span>
+    );
+
+    const [hoverable, hovered] = useHover(element);
+    return (
+      <>
+        {hoverable}
+        <span>{hovered ? "HOVERED!" : ""}</span>
+      </>
+    );
+  },
+};
