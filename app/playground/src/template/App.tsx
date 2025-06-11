09ea132d72acaf483d38070fea719483cc8fe0d1
ZHH <zhang2ha@qq.com>
2025-06-11 16:03:52 +0800

    playground 使用 npm 上发布的组件库

--- app/playground/src/template/App.tsx
+++ app/playground/src/template/App.tsx
@@ -1,19 +1,24 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react'
+import './App.css'
+import { Button } from '@guang-comp/react-comp-lib'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Hello World</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
       </div>
+      <Button
+        type="default"
+        onClick={() => alert(1)}
+      >
+        按钮二
+      </Button>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- packages/hooks/rollup.config.mjs
+++ packages/hooks/rollup.config.mjs
@@ -1,4 +1,3 @@
-import postcss from 'rollup-plugin-postcss'
 import typescript from '@rollup/plugin-typescript'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
@@ -22,9 +21,6 @@ export default {
     commonjs(),
     typescript({
       tsconfig: 'tsconfig.json',
-    }),
-    postcss({
-      extract: true,
     }),
   ],
 }
